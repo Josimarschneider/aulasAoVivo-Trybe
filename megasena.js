0047ed0006788b23a2145d5983a6a764cbe3e148
Josimar Schneider <josimarschneider@gmail.com>
2021-04-26 16:08:05 -0400

    criando repositorio

---
+++ megasena.js
@@ -0,0 +1,13 @@
+let megaSenaNumbers = [];
+
+let firstNumber =Math.ceil(Math.random() * 60);;
+let secondNumber =Math.ceil(Math.random() * 60);;
+let thirdNumber =Math.ceil(Math.random() * 60);;
+let fourNumber =Math.ceil(Math.random() * 60);;
+let fifthMu,ber =Math.ceil(Math.random() * 60);;
+let sixNumber =Math.ceil(Math.random() * 60);;
+
+//onsole.log(Math.random() * 60);
+//console.log(Math.ceil())
+
+
