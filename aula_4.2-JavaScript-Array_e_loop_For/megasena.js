4b6c0192669b4fbe563c018fea152f9687fda102
Josimar Schneider <josimarschneider@gmail.com>
2021-04-27 14:33:17 -0400

    incrementado pra mais de um jogador

--- aula_4.2-JavaScript-Array_e_loop_For/megasena.js
+++ aula_4.2-JavaScript-Array_e_loop_For/megasena.js
@@ -10,17 +10,26 @@ let sixthNumber = Math.ceil(Math.random() * 60);
 megaSenaNumbers = [firstNumber, secondNumber, thirdNumber, fourthNumber, firstNumber, sixthNumber];
 
 let cartelaNumbers = [20, 35, 23 ,48 ,3 ,17];
+let cartelaNumbers1 = [21, 33, 28 ,43 ,34 ,19];
+let cartelaNumbers2 = [25, 37, 25 ,47 ,38 ,12];
 
-let acertos = 0;
+let cartelasJogos = [cartelaNumbers, cartelaNumbers1, cartelaNumbers2];
 
-for(indexMega = 0; indexMega < megaSenaNumbers.length; indexMega += 1){
-    for (let indexCart= 0;indexCart < cartelaNumbers.length; indexCart += 1) {
-        if (megaSenaNumbers [indexMega] === cartelaNumbers[indexCart]){
-            acertos += 1;
+
+
+for (let indexjogos = 0; indexjogos < cartelasJogos.length; indexjogos += 1){
+    let acertos = 0;
+    for(indexMega = 0; indexMega < megaSenaNumbers.length; indexMega += 1){
+        for (let indexCart= 0;indexCart < cartelasJogos[indexjogos].length; indexCart += 1) {
+            if (megaSenaNumbers [indexMega] === cartelasJogos[indexjogos][indexCart]){
+                acertos += 1;
+            };
         };
     };
-};
-
-console.log(cartelaNumbers);
-console.log(megaSenaNumbers);
-console.log(acertos);
+    console.log('jogada ' + (indexjogos +1) );
+    console.log('mega-sena: ' + megaSenaNumbers );
+    console.log('jogo: ' + cartelasJogos[indexjogos] );
+    console.log('Acertos: ' + acertos );
+    console.log('***************************** ' );
+  
+}
